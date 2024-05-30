6adc9d64ca1cec6bbb7a6e46a09a59f8871c18e8
John W. Leimgruber III <leimgrub@gmail.com>
2024-05-30 17:11:55 -0400

    WIP cleaning up some more in the library

--- src/llama_cpp_api_client/client.py
+++ src/llama_cpp_api_client/client.py
@@ -1,11 +1,20 @@
 import asyncio
 import json
 from collections.abc import AsyncGenerator
+import sys
 
 from aiohttp import ClientSession
 
 # Default Address and Port of your LLaMA.cpp HTTP Server
 DEFAULT_BASE_URL = "http://localhost:8080"
+
+# Default headers for HTTP POSTs/GETs
+DEFAULT_HEADERS = {
+    "User-Agent": "aiohttp",
+    "Content-Type": "application/json",
+    "Connection": "keep-alive",
+    "Accept": "text/event-stream",
+}
 
 # Default options for POST /completion API endpoint
 # https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md#api-endpoints
@@ -28,50 +37,47 @@ DEFAULT_COMPLETION_OPTIONS = {
     "stream": True,
 }
 
-# Default headers for HTTP POSTs/GETs
-DEFAULT_HEADERS = {
-    "User-Agent": "aiohttp",
-    "Content-Type": "application/json",
-    "Connection": "keep-alive",
-    "Accept": "text/event-stream",
-}
+# LLaMA.cpp HTTP Server Response Body Start String
+# https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md#result-json
+DEFAULT_RESPONSE_BODY_START_STRING = "data: ".encode("utf-8")
 
 
-async def stream_response(base_url: str = DEFAULT_BASE_URL, data: dict = {}) -> AsyncGenerator[dict, None]:
+async def stream_response(
+    base_url: str = DEFAULT_BASE_URL, options: dict = {}, headers: dict = {}
+) -> AsyncGenerator[dict, None]:
     """
-    Stream llamma.cpp server /completion API full responses.
-    TODO: try/catch/raise errors
+    Stream LLaMA.cpp HTTP Server API POST /completion response
     """
-    async with ClientSession() as session:
-        headers = DEFAULT_HEADERS
-        data = DEFAULT_COMPLETION_OPTIONS
+    try:
+        async with ClientSession() as session:
+            # override defaults with whatever userland passes
+            combined_headers = DEFAULT_HEADERS
+            combined_headers.update(headers)
+            combined_options = DEFAULT_COMPLETION_OPTIONS
+            combined_options.update(options)
 
-        url = f"{base_url.rstrip("/")}/completion"
-        print(f"API Endpoint: {url}")
-        # FIXME: this next line does not work if it isn't already set before passing in the dict
-        data["stream"] = True
+            url = f"{base_url.rstrip("/")}/completion"
 
-        async with session.post(url, headers=headers, json=data) as response:
-            if not response.status == 200:
-                print(response.status)
+            async with session.post(url=url, headers=combined_headers, json=combined_options) as response:
+                if not response.status == 200:
+                    print(f"HTTP Response: {response.status}")
 
-            async for raw_line in response.content:
-                if len(raw_line) == 1:
-                    continue
-                if raw_line[:6] != "data: ".encode("utf-8"):
-                    print("Invalid data header...")
-                    continue
-                line = raw_line.decode("utf-8")[6:]
-                yield json.loads(line)
+                async for raw_line in response.content:
+                    if len(raw_line) == 1:
+                        continue
+                    if raw_line[: len(DEFAULT_RESPONSE_BODY_START_STRING)] != DEFAULT_RESPONSE_BODY_START_STRING:
+                        # FIXME: this is brittle code, not sure if another json decoder and skip the "data: " part...
+                        print("Invalid response body starting string, unable to parse response...")
+                        continue
+                    line = raw_line.decode("utf-8")[len(DEFAULT_RESPONSE_BODY_START_STRING) :]
+                    yield json.loads(line)
+    except Exception as e:
+        raise e
 
 
 async def main() -> None:
-
-    # system_prompt = "You are a Zen master and mystical poet."
-    # user_prompt = "Write a simple haiku about being Autistic."
-
-    system_prompt = "You are an experienced Python software engineer working in a Linux development environment."
-    user_prompt = "Suggest a good name for a Python library that providing a streaming API client to llama.cpp server."
+    system_prompt = "You are a Zen master and mystical poet."
+    user_prompt = "Write a simple haiku about llamas."
 
     # >>> ChatML Prompt Template
     # "prompt": f"<|im_start|>system\n{system_prompt}\n<|im_end|>\n<|im_start|>user\n{user_prompt}\n<|im_end|>\n<|im_start|>assistant\n",
@@ -79,16 +85,17 @@ async def main() -> None:
     # >>> Llama-3-70B Prompt Template
     # "prompt": f"<|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{user_prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
 
-    complete = ""
+    total = ""
     async for response in stream_response():
         if response.get("stop", False):
             print(f">>> Timings:\n{response["timings"]}")
             print(f">>> Prompt:\n{response["prompt"]}")
             continue
-        complete += response["content"]
-        print(response["content"])
+        total += response["content"]
+        print(response["content"], end="")
+        sys.stdout.flush()
 
-    print(f">>> Response:\n{complete}")
+    print(f">>> Response:\n{total}")
 
 
 if __name__ == "__main__":
